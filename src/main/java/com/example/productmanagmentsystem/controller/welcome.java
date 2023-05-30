d523a54a77ed6bfa8eb2772d3eae07b6088c526b
Anubhav Agnihotri <anubahvagnihotri@gmail.com>
2023-05-30 16:34:47 +0530

    Login Page Credentials and control added with welcome page.

--- src/main/java/com/example/productmanagmentsystem/controller/welcome.java
+++ src/main/java/com/example/productmanagmentsystem/controller/welcome.java
@@ -2,7 +2,7 @@ package com.example.productmanagmentsystem.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.thymeleaf.standard.expression.GreaterThanExpression;
+
 
 @RequestMapping(value = "welcome",method = RequestMethod.GET)
 public class welcome {
