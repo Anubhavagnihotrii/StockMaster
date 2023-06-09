85a8dca34adee50cd4fcd4b2064567e952562b58
Anubhav Agnihotri <anubahvagnihotri@gmail.com>
2023-06-10 01:01:52 +0530

    added some other users who can login to website

--- src/main/java/com/example/productmanagmentsystem/controller/login.java
+++ src/main/java/com/example/productmanagmentsystem/controller/login.java
@@ -1,7 +1,6 @@
 package com.example.productmanagmentsystem.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class login {
     @GetMapping("/login")
-    public String login()
+    public String dologin()
     {
         System.out.println("Inside the Login Page");
         return "login";
