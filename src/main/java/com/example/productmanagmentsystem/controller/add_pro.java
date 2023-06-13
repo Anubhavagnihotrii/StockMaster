f442d93ba77076d8ac585a02075cf9b12ffa3e30
Anubhav Agnihotri <anubahvagnihotri@gmail.com>
2023-06-14 00:36:38 +0530

    added the alerts to all the places where they are required.

--- src/main/java/com/example/productmanagmentsystem/controller/add_pro.java
+++ src/main/java/com/example/productmanagmentsystem/controller/add_pro.java
@@ -1,6 +1,7 @@
 package com.example.productmanagmentsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,30 +11,33 @@ import java.io.IOException;
 
 @Controller
 public class add_pro {
-  @GetMapping("addpro")
-    public String addproducts()
+  @GetMapping("add-pro")
+    public String add_products()
   {
     System.out.println("inside add pro page");
-      return "addpro";
+      return "add-pro";
   }
-  @PostMapping("addpro")
-    public void products_insertion(@RequestParam("product-name") String productname, @RequestParam("email")String email)
+  @PostMapping("add-pro")
+    public String products_insertion(@RequestParam("product-code") String product_code,@RequestParam("product-name") String product_name,@RequestParam("product-company") String product_company, @RequestParam("product-price")long product_price,@RequestParam("product-quantity")long product_quantity,
+  Model model)
   {
-    System.out.println(productname);
-    System.out.println(email);
+    System.out.println("Inside products entry form");
+    System.out.println(product_code+product_name+product_company+product_price+product_quantity);
 
     try{
-      String data ="product name: "+productname+" email: "+email;
-      FileWriter fw = new FileWriter("add-products.txt",true);
+      String data ="product_code:"+product_code+" product_name:"+product_name+" product_company:"+product_company+" price:"+product_price+" Quantity:"+product_quantity;
+      FileWriter fw = new FileWriter("products.txt",true);
 
       fw.write("\n"+data);
       fw.close();
       System.out.println("data inserted successfully");
 
+      model.addAttribute("success","Data Inserted Successfully !");
     }
     catch (IOException e) {
       e.printStackTrace();
       System.out.println("An error occurred while storing the form data: " + e.getMessage());
     }
+    return "add-pro";
   }
 }
--- src/main/java/com/example/productmanagmentsystem/controller/login.java
+++ src/main/java/com/example/productmanagmentsystem/controller/login.java
@@ -1,6 +1,7 @@
 package com.example.productmanagmentsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,7 @@ public class login {
         return "login";
     }
     @PostMapping("login")
-    public String login_val(@RequestParam("username") String username , @RequestParam("password") String password)
+    public String login_val(@RequestParam("username") String username , @RequestParam("password") String password, Model model)
     {
         System.out.println("Inside the login form click");
 
@@ -28,10 +29,13 @@ public class login {
         if(username.equals(u1) && password.equals(p1) || username.equals(u2) && password.equals(p2) || username.equals(u3) && password.equals(p3))
         {
             System.out.println("inside the welcome page");
+            model.addAttribute("success","Logged in Sucessfully !");
             return "/welcome";
+
         }
         else
         {
+            model.addAttribute("failure","Log in Failed please try again");
             return "/login";
 
         }
